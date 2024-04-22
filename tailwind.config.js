b43fbcfad51d7583c093d3fd2db7e9773e36eb0f
shravan <shravan.sathyanarayanan@gmial.com>
2024-04-22 09:25:52 +0530

    meow 12

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html','./signin.html','./main_page.html'],
+  content: ['./index.html','./signin.html','./main_page.html','./animal1.html'],
   theme: {
     extend: {},
   },
